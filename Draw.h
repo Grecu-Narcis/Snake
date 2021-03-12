dc47e8cf21c91119198c07452fe8e5050291137d
Narcis Grecu <68643190+Narcis09@users.noreply.github.com>
2021-03-12 12:59:36 +0200

    Update Draw.h

--- Draw.h
+++ Draw.h
@@ -3,5 +3,5 @@
 using namespace sf;
 void drawHorizontalLine(int x, int y, RenderWindow& window);
 void drawVerticalLine(int x, int y, RenderWindow& window);
-void drawTable(RenderWindow& window);
-void drawSquare(int x, int y, Color color, RenderWindow& window);
+void drawTable(RenderWindow& window, int tileSize);
+void drawSquare(int x, int y, Color color, RenderWindow& window, int size);
