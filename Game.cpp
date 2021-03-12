5f12c23ad2d7d1a0f701c00e595de7bbc9e17a96
Narcis Grecu <68643190+Narcis09@users.noreply.github.com>
2021-03-12 13:00:22 +0200

    Update Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -1,20 +1,111 @@
-#include <cstdlib>
-#include <ctime>
-#include <iostream>
-#include "Draw.h"
+#include "Game.h"
 
 using namespace std;
 
-pair<int, int> generateFood() {
+Game::Game() {
+	width = N * tileSize, height = M * tileSize + 70;
+	generateFood();
+	loadHighScore("scor.txt");
+	font.loadFromFile("arial.ttf");
+	scoreText.setFillColor(Color::White);
+	scoreText.setFont(font);
+	scoreText.setCharacterSize(30);
+	scoreText.setPosition(Vector2f(0, height - 70));
+	scoreText.setString("Your score: " + to_string(score));
+
+	highScoreText = scoreText;
+	highScoreText.setPosition(Vector2f(width - 240, height - 70));
+	highScoreText.setString("High score: " + to_string(highScore));
+}
+
+
+
+void Game::generateFood() {
 	srand(time(0));
 	int x, y;
 
-	x = rand() % 12;
-	y = rand() % 8;
+	do {
+		x = rand() % 12;
+		y = rand() % 8;
+	} while (!snake.isEmpty({ x, y }));
 
-	return make_pair(x, y);
+	food = make_pair(x, y);
 }
 
-void drawFood(pair<int, int> Food, RenderWindow& window) {
-	drawSquare(Food.first * 50 + 5, Food.second * 50 + 5, Color::Green, window);
+pair<int, int> Game::getFood() {
+	return food;
+}
+
+void Game::displayFood(RenderWindow& window) {
+	drawSquare(food.first * tileSize + 5, food.second * tileSize + 5, Color::Yellow, window, tileSize);
+}
+
+void Game::loadHighScore(string path) {
+	ifstream file(path);
+	if (file.peek() == EOF)
+		highScore = 0;
+	else
+		file >> highScore;
+
+	file.close();
+}
+
+void Game::updateScore() {
+	score++;
+
+	setScoreText("Your score: " + to_string(score));
+
+	if (score > highScore) {
+		highScore = score;
+		setHighScoreText("High score: " + to_string(highScore));
+	}
+}
+
+void Game::updateHighScore(string path) {
+	ofstream file(path);
+	file << highScore;
+	file.close();
+}
+
+void Game::displayScores(RenderWindow& window) {
+	window.draw(scoreText);
+	window.draw(highScoreText);
+}
+
+
+void Game::displaySnake(RenderWindow& window) {
+	snake.drawSnake(window, tileSize);
+}
+
+void Game::updateSnake(RenderWindow& window, int direction) {
+	snake.moveSnake(direction);
+
+	if (snake.hasLost(window, tileSize))
+		gameOver = true;
+
+	else if (snake.hasAteFood(food)) {
+		snake.addPieceToBody();
+		generateFood();
+		updateScore();
+	}
+}
+
+bool Game::getGameStatus() {
+	return gameOver;
+}
+
+void Game::setScoreText(string s) {
+	scoreText.setString(s);
+}
+
+void Game::setHighScoreText(string s) {
+	highScoreText.setString(s);
+}
+
+int Game::getScore() {
+	return score;
+}
+
+int Game::getHighScore() {
+	return highScore;
 }
