5ab2df16386197f2373328c25ae76a948e7c8f92
Narcis Grecu <68643190+Narcis09@users.noreply.github.com>
2021-03-21 21:44:52 +0200

    Delete Draw.h

--- Draw.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <SFML/Graphics.hpp>
-using namespace sf;
-void drawHorizontalLine(int x, int y, RenderWindow& window);
-void drawVerticalLine(int x, int y, RenderWindow& window);
-void drawTable(RenderWindow& window, int tileSize);
-void drawSquare(int x, int y, Color color, RenderWindow& window, int size);
